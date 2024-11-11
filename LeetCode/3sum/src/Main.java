3768646b3599d9df9af7e667fd0a5c559a88efcb
amr mahmoud <94997694+AmrMahmoudSaid@users.noreply.github.com>
2024-11-11 21:37:46 +0200

    3sum

---
+++ LeetCode/3sum/src/Main.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> res = threeSum(nums);
+        System.out.println(res);
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    if (!res.contains(Arrays.asList(nums[i], nums[left], nums[right]))){
+                        res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    }
+
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return res;
+    }
+}
